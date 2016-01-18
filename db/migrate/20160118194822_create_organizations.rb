82367c2eeb5d16cc12994ca2a05a8a8c61bf93c5
Adam Wilbert <adam.wilbert@gmail.com>
2016-01-18 11:49:18 -0800

    initial commit

---
+++ db/migrate/20160118194822_create_organizations.rb
@@ -0,0 +1,16 @@
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :email
+      t.string :description
+      t.string :profile_image
+      t.string :address
+      t.string :phone
+      t.string :zip
+      t.string :website
+
+      t.timestamps null: false
+    end
+  end
+end
